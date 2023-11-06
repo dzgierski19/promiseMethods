e3a5bac5a8cdf8705192af8ffd097701e27ae377
dzgierski19 <dzgierskigit@gmail.com>
2023-11-06 15:14:07 +0100

    changed logic

--- PromiseLast.ts
+++ PromiseLast.ts
@@ -2,7 +2,7 @@ import { PROMISEARRAY, isArrayEmpty } from "./Global";
 
 // Develop a function promise.last(arrayOfPromises).
 
-const promiseAllArr = <T>(arrayOfPromises: Promise<T>[]): Promise<T[]> => {
+const promiseLast = <T>(arrayOfPromises: Promise<T>[]): Promise<T> => {
   isArrayEmpty(arrayOfPromises);
   const arr = [] as T[];
   // return new Promise((resolve, reject) => {
@@ -16,11 +16,16 @@ const promiseAllArr = <T>(arrayOfPromises: Promise<T>[]): Promise<T[]> => {
   //   });
   // });
   return new Promise((resolve, reject) => {
+    let resolvedPromiseCounter = 0;
+    let promiseCounter = 0;
     for (let i = 0; i < arrayOfPromises.length; i++) {
+      promiseCounter++;
       arrayOfPromises[i]
         .then((value) => {
-          arr.push(value);
-          resolve(arr);
+          resolvedPromiseCounter++;
+          if (resolvedPromiseCounter === 3) {
+            resolve(value);
+          }
         })
         .catch((error) => {
           throw error;
@@ -29,15 +34,15 @@ const promiseAllArr = <T>(arrayOfPromises: Promise<T>[]): Promise<T[]> => {
   });
 };
 
-const promiseLast = <T>(arrayOfPromises: Promise<T>[]): Promise<T> => {
-  return promiseAllArr(arrayOfPromises).then((data) => {
-    if (data.length) {
-      console.log(data.length);
-      return data[data.length - 1];
-    }
-    throw new Error("array error");
-  });
-};
+// const promiseLast = <T>(arrayOfPromises: Promise<T>[]): Promise<T> => {
+//   return promiseAllArr(arrayOfPromises).then((data) => {
+//     if (data.length) {
+//       console.log(data.length);
+//       return data[data.length - 1];
+//     }
+//     throw new Error("array error");
+//   });
+// };
 
 // async function promiseRaceAsync() {
 //   const result = await promiseAllArr(PROMISEARRAY);
@@ -46,13 +51,15 @@ const promiseLast = <T>(arrayOfPromises: Promise<T>[]): Promise<T> => {
 //   return lastItem;
 // }
 
+console.time("A");
 const promiseLastResult = promiseLast(PROMISEARRAY);
 console.log(promiseLastResult);
 
 setTimeout(() => {
   promiseLastResult.then((data) => {
     console.log(data);
+    console.timeEnd("A");
   });
-}, 6000);
+}, 1000);
 
 //In Promise.all the order of the promises is maintained in the values variable, irrespective of which promise was first resolved.
