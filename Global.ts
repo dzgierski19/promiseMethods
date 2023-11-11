dfb18354c2bd9bb806ffb484ab9449abb7e572d6
dzgierski19 <dzgierskigit@gmail.com>
2023-11-11 19:46:29 +0100

    deleted isArrayEmpty

--- Global.ts
+++ Global.ts
@@ -1,17 +1,19 @@
-export const PROMISE1 = new Promise<string>((resolve, reject) => {
-  setTimeout(() => resolve("First Promise Resolved"), 2000);
-});
-export const PROMISE2 = new Promise<string>((resolve, reject) => {
-  setTimeout(() => resolve("Second Promise Resolved"), 3000);
-});
-export const PROMISE3 = new Promise<string>((resolve, reject) => {
-  setTimeout(() => resolve("Third Promise Resolved"), 1000);
-});
-
-export const PROMISEARRAY = [PROMISE1, PROMISE2, PROMISE3];
-
-export const isArrayEmpty = <T>(array: Promise<T>[]) => {
-  if (!array.length) {
-    throw new Error("Please type an array with at least one promise");
-  }
+const promise1 = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => reject("First Promise Resolved"), 2000);
+  });
 };
+
+const promise2 = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => reject("Second Promise Resolved"), 3000);
+  });
+};
+
+const promise3 = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve("Third Promise Resolved"), 1000);
+  });
+};
+
+export const PROMISEARRAY = [promise1, promise2, promise3];
