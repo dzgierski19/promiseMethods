7481534df83af08524fd91d615380a4f5e64ded6
dzgierski19 <dzgierskigit@gmail.com>
2023-11-11 19:46:10 +0100

    solution added

--- PromiseIgnoreErrors.ts
+++ PromiseIgnoreErrors.ts
@@ -1,1 +1,49 @@
 // Develop a function promise.ignoreErrors(arrayOfPromises).
+
+import { PROMISEARRAY } from "./Global";
+
+const promiseIgnoreErrors = <T>(
+  arrayOfPromises: (() => Promise<T>)[]
+): Promise<T[]> => {
+  return new Promise((resolve, reject) => {
+    let promiseCounter = 0;
+    const resultArray = [] as T[];
+    arrayOfPromises.forEach((element) => {
+      element()
+        .then((data) => {
+          promiseCounter++;
+          resultArray.push(data);
+          if (promiseCounter === arrayOfPromises.length) {
+            resolve(resultArray);
+          }
+        })
+        .catch(() => {
+          promiseCounter++;
+          if (promiseCounter === arrayOfPromises.length) {
+            resolve(resultArray);
+          }
+        });
+    });
+  });
+};
+
+const promiseIgnoreErrorsAsyncAwait = async <T>(
+  arrayOfPromises: (() => Promise<T>)[]
+): Promise<T[]> => {
+  const resultArray = [] as T[];
+  for (const element of arrayOfPromises) {
+    try {
+      const result = await element();
+      resultArray.push(result);
+    } catch (error) {}
+  }
+  return resultArray;
+};
+
+promiseIgnoreErrorsAsyncAwait(PROMISEARRAY)
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
