986120363bec5cbcef18f081867052215bd49284
dzgierski19 <dzgierskigit@gmail.com>
2023-11-11 19:01:34 +0100

    async/await solution

--- PromiseAll.ts
+++ PromiseAll.ts
@@ -2,70 +2,51 @@ import { PROMISEARRAY, isArrayEmpty } from "./Global";
 
 // Develop a function promise.all(arrayOfPromises).
 
-const promiseAll = <T>(arrayOfPromises: Promise<T>[]): Promise<T[]> => {
-  isArrayEmpty(arrayOfPromises);
+//PROMISES
+
+const promiseAll = <T>(arrayOfPromises: (() => Promise<T>)[]): Promise<T[]> => {
+  // isArrayEmpty(arrayOfPromises);
   return new Promise((resolve, reject) => {
     const arr = [] as T[];
-    arrayOfPromises.forEach((element) => {
-      element
+    let counter = 0;
+    arrayOfPromises.forEach((element, index) => {
+      element()
         .then((value) => {
-          arr.push(value);
-          resolve(arr);
+          counter++;
+          arr[index] = value;
+          if (counter === arrayOfPromises.length) {
+            resolve(arr);
+          }
         })
         .catch((error) => {
           reject(error);
-          throw error;
         });
     });
   });
 };
 
-const promiseAllAsyncAwait = async <T>(arrayOfPromises: Promise<T>[]) => {
-  isArrayEmpty(arrayOfPromises);
+//ASYNC/AWAIT
+
+const promiseAllAsyncAwait = async <T>(
+  arrayOfPromises: (() => Promise<T>)[]
+): Promise<T[]> => {
+  // isArrayEmpty(arrayOfPromises);
   const arr = [] as T[];
-  // for (const element of arrayOfPromises) {
-  arrayOfPromises.forEach(async (element) => {
+  for (const element of arrayOfPromises) {
     try {
-      const value = await element;
-      arr.push(value);
+      const transfer = await element();
+      arr.push(transfer);
     } catch (error) {
       throw error;
     }
-  });
+  }
   return arr;
 };
 
-const a = promiseAllAsyncAwait(PROMISEARRAY);
-
-console.log(a);
-
-setTimeout(() => console.log(a), 4000);
-
-const promiseAllArrayFrom = <T>(
-  arrayOfPromises: Promise<T>[]
-): Promise<T[] | void> => {
-  isArrayEmpty(arrayOfPromises);
-  return new Promise((resolve, reject) => {
-    const arr = [] as T[];
-    Array.from(arrayOfPromises, (element) => {
-      element
-        .then((value) => arr.push(value))
-        .catch((error) => {
-          reject(error);
-        });
-    });
-    resolve(arr);
+promiseAllAsyncAwait(PROMISEARRAY)
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((e) => {
+    console.log(e);
   });
-};
-
-// const promiseAllArrayFromResult = promiseAllArrayFrom(PROMISEARRAY);
-// console.log(promiseAllArrayFromResult);
-
-// setTimeout(() => console.log(promiseAllArrayFromResult), 4000);
-
-async function promiseAllAsync() {
-  const result = await promiseAll(PROMISEARRAY);
-  //   console.log(result[result.length - 1] + "2");
-  //   console.log(result);
-  return result;
-}
