5c130f76818b3135f638001d45953f1b584570b8
dzgierski19 <dzgierskigit@gmail.com>
2023-11-11 21:09:54 +0100

    solution added

--- PromiseAllSettled.ts
+++ PromiseAllSettled.ts
@@ -2,7 +2,57 @@
 
 import { PROMISEARRAY } from "./Global";
 
-const result = Promise.allSettled(PROMISEARRAY);
-console.log(result);
+type PromiseType<T> = {
+  status: "fulfilled" | "rejected";
+  value?: T;
+  reason?: any;
+};
 
-setTimeout(() => console.log(result), 4000);
+const promiseAllSettled = <T>(
+  arrayOfPromises: (() => Promise<T>)[]
+): Promise<PromiseType<T>[]> => {
+  return new Promise((resolve) => {
+    const arr = [] as PromiseType<T>[];
+    let counter = 0;
+    arrayOfPromises.forEach((element, index) => {
+      element()
+        .then((result) => {
+          counter++;
+          arr.push({ status: "fulfilled", value: result });
+          if (counter === arrayOfPromises.length) {
+            resolve(arr);
+          }
+        })
+        .catch((error) => {
+          counter++;
+          arr.push({ status: "rejected", reason: error });
+          if (counter === arrayOfPromises.length) {
+            resolve(arr);
+          }
+        });
+    });
+  });
+};
+
+const promiseAllSettledAsyncAwait = async <T>(
+  arrayOfPromises: (() => Promise<T>)[]
+) => {
+  const array = [] as PromiseType<T>[];
+  for (const element of arrayOfPromises) {
+    try {
+      const result = await element();
+      array.push({ status: "fulfilled", value: result });
+    } catch (error) {
+      array.push({ status: "rejected", reason: error });
+    }
+  }
+  return array;
+};
+
+promiseAllSettledAsyncAwait(PROMISEARRAY)
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
